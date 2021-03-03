76da45e0efc483b05a7bf4ac68abffe40fd09377
Trevor Thai Kim Nguyen <trevortknguyen@utexas.edu>
2021-03-03 16:01:00 -0600

    Add some code

---
+++ scripts/2021_02_04.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 11:40:23 2021
+
+@author: trevortknguyen
+"""
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+plt.style.use('ggplot')
+
+#%%
+from src.parameters import ANIMALS
+from loren_frank_data_processing import make_epochs_dataframe
+from loren_frank_data_processing import make_neuron_dataframe
+from loren_frank_data_processing import get_position_dataframe
+from loren_frank_data_processing import get_spikes_dataframe
+
+
+#%%
+epoch_info = make_epochs_dataframe(ANIMALS)
+epoch_key = ('HPa', 3, 2) # animal, day, epoch
+
+#%%
+
+# avoid running this as a cell
+position_info = get_position_dataframe(epoch_key, ANIMALS)
+
+#%%
+
+position_info['time_sec'] = position_info.index / np.timedelta64(1, 's')
+
+#%%
+
+neuron_info = make_neuron_dataframe(ANIMALS)
+neuron_info = neuron_info.xs(epoch_key, drop_level=False)
+
+#%%
+
+def get_position_info_indices_at_spikes(spike_times_sec, position_info):
+    '''
+    spike_times_sec must be the times in seconds
+    position_info must be modified to have a 'time_sec' column with corresponding units to spike_times_sec
+    '''
+    time_indices = np.empty((spike_times_sec.shape[0]), dtype='int')
+
+    spike_time_index = 0
+
+    for i, spike_time in enumerate(spike_times_sec):
+        for index in range(spike_time_index, position_info['time_sec'].shape[0]):
+            prev_index = max(0, index-1)
+            prev_pos_time = position_info['time_sec'][prev_index]
+            pos_time = position_info['time_sec'][index]
+            # if we have gone too far
+            if pos_time > spike_time:
+                spike_time_index = prev_index
+                break
+        # time_ind (where to index into positions table)
+        time_indices[i] = spike_time_index
+    return time_indices
+
+#%%
+
+# create the bins
+
+spike_counts_linear_position = np.zeros(163)
+occupancy_time_linear_position = np.zeros(163)
+
